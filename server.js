fdc3c0e820f9ca0265a9f1d4dd6b078029b2c65a
Malik Öztürk <malik.ozturk@enuygun.com>
2023-12-13 01:21:27 +0300

    feat: add server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const { loadEnvConfig } = require('@next/env')
 const Backend = require('i18next-fs-backend')
+const { realpathSync } = require('fs')
 const { initReactI18next } = require('react-i18next')
+const { resolve } = require('path')
 
 const { options, default: i18n } = require('./dist/utils/i18n')
 
 const projectDir = process.cwd()
 loadEnvConfig(projectDir)
+const appDirectory = realpathSync(process.cwd())
+const resolveApp = (relativePath) => resolve(appDirectory, relativePath)
+const appSrc = resolveApp('public')
 
 i18n
   .use(Backend)
