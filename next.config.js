90693e9cebad157f4c36e1e5c7eae34db295bcc0
Malik Öztürk <malik.ozturk@enuygun.com>
2023-12-13 01:11:36 +0300

    add next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 const { i18n } = require('./next-i18next.config')
+const { loadEnvConfig } = require('@next/env')
 
+const projectDir = process.cwd()
+loadEnvConfig(projectDir)
+
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {
-  i18n,
+  i18n: {
+    ...i18n,
+  },
+  trailingSlash: true,
+  generateEtags: false,
+  poweredByHeader: false,
+  productionBrowserSourceMaps: false,
+  compress: false,
+  swcMinify: true,
   compiler: {
     styledComponents: true,
   },
