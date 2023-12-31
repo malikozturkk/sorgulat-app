5ccb168ccd7728e269c27203ba1baeb0dbd6b2e4
Malik Öztürk <malik.ozturk@enuygun.com>
2023-12-31 16:47:39 +0300

    fix: react-i18 next problem

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,14 +1,18 @@
-  module.exports = {
+/** @type {import('next-i18next').UserConfig} */
+module.exports = {
+  fallbackLng: 'tr',
+  supportedLngs: ['tr', 'en'],
+  load: 'languageOnly',
+  ns: ['common'],
+  nonExplicitSupportedLngs: true,
+  defaultNS: 'common',
   i18n: {
     locales: ['tr', 'en'],
     defaultLocale: 'tr',
-    localeDetection: false,
+    localeDetection: false
   },
-  serializeConfig: false,
-  lowerCaseLng: true,
-  fallbackLng: (code) => {
-    const fallbacks = []
-    fallbacks.push(code)
-    return fallbacks
+  fallbackLng: {
+    default: ['tr'],
   },
+  
 }
